dd6cbc32eb1d1be8d6898287e631246d669f52fc
N.Upendhar <80301044+Upendhar10@users.noreply.github.com>
2025-08-01 22:19:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function firstWord(s) {
   // your code here
 
-	if(s.trim() === " " || typeof s !== "string") return "";
+	if(typeof s !== "string" || s.trim() === "") return "";
 
+	str = str.trim();
 	const spaceIdx = s.indexOf(" "); // returns index of 1st empty char
 	return spaceIdx === -1 ? s : s.slice(0, spaceIdx);
 }
