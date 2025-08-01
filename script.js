17ea633112ce004051798457e155fb7c850507bf
N.Upendhar <80301044+Upendhar10@users.noreply.github.com>
2025-08-01 22:14:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function firstWord(s) {
   // your code here
+
+	if(s.trim() === " " || typeof s !== "string") return "";
+
+	const spaceIdx = s.indexOf(" "); // returns index of 1st empty char
+	return spaceIdx === -1 ? s : s.slice(0, spaceIdx);
 }
 
 // Do not change the code below
